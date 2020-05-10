a11c15a2a481920b4d82af3975ec6080a253eb1c
adams66 <adamsdalton14@gmail.com>
2020-05-09 23:32:26 -0600

    git commit

---
+++ Assignment3/script.js
@@ -0,0 +1,7 @@
+function hamburger(){
+document.getElementById("menu").classList.toggle("change");
+}
+
+function chicken(){
+    document.getElementById("chickenNoodle").classList.toggle("changeC");
+}
