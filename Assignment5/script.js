a7663f47ce3471d7e80ea118447e39f95646e743
adams66 <adamsdalton14@gmail.com>
2020-05-26 01:02:28 -0600

    git commit

---
+++ Assignment5/script.js
@@ -0,0 +1,19 @@
+
+
+function hamburger(){
+document.getElementById("menu").classList.toggle("change");
+document.getElementById("beta").classList.toggle("change-nav");
+
+}
+
+function myFunction(){  
+
+    var e = new Date();
+
+
+
+document.getElementById("time").innerHTML = e;
+
+
+}
+
