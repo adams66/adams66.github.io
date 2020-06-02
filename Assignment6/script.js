72c66a9bb6cebae317249dce01e662f91e1dfe2f
adams66 <adamsdalton14@gmail.com>
2020-06-02 12:20:55 -0600

    git commit

--- Assignment6/script.js
+++ Assignment6/script.js
@@ -9,23 +9,26 @@ document.getElementById("beta").classList.toggle("change-nav");
 function myFunction(){  
 
     var e = new Date();
-    
+    var w = e.getDay();
+
+    if(w == 3){
+    var myElement = document.querySelector("#banner");
+    myElement.style.height = "200px";
+    myElement.style.margin = "25px";
+     
+   
+    }
+
+ else{
+   
+  }
 
 
 
 document.getElementById("time").innerHTML = e;
 
 
+
 }
-
-function event(){
-    let w = getDay();
-        if(w == 4)
-        {
-
-        }
- 
-}
-
 
 
