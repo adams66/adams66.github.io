cee2d8d6f43536d26181543dbdb53257e838e132
adams66 <adamsdalton14@gmail.com>
2020-07-14 09:17:36 -0600

    git commit

---
+++ AssigmentFinal/Javascript/scriptStorm.js
@@ -0,0 +1,30 @@
+
+
+function hamburger(){
+document.getElementById("menu").classList.toggle("change");
+document.getElementById("beta").classList.toggle("change-nav");
+
+}
+
+function myFunction(){  
+
+  var e = new Date();
+  var w = e.getDay();
+
+
+
+
+
+
+
+
+
+
+document.getElementById("time").innerHTML = e;
+
+
+
+}
+
+
+
