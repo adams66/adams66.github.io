62a80c9bd3bf8387df76f44feabcfca402aabcb3
adams66 <47400311+adams66@users.noreply.github.com>
2023-05-04 15:52:58 -0600

    the start of homebase

--- app/src/App.js
+++ app/src/App.js
@@ -7,6 +7,18 @@ function App() {
 	return (
 		<main class="container-fluid">
 			<Navbar />
+			<div class="grid">
+				<div>
+					<article>
+						<a href="https://adams66.github.io/home/">Portfoilo</a>
+					</article>
+				</div>
+				<div>
+					<article>
+						<a href="https://adams66.github.io/royal-wildcat/">Royal Wildcat</a>
+					</article>
+				</div>
+			</div>
 		</main>
 	);
 }
